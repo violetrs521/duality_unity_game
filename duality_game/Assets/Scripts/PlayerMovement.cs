a94c2d0a3d5fe717f0900a4932df3d886fd53826
chris-m-taylor <chrismtaylor1998@gmail.com>
2022-01-28 21:27:47 -0500

    .gitignore fix

---
+++ duality_game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidBody;
+    private Vector3 change;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+
+    void FixedUpdate()
+    {       
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+
+        Move();
+    }
+
+    void Move()
+    {
+        if (change != Vector3.zero)
+        {
+            myRigidBody.MovePosition(transform.position + change.normalized * (Time.deltaTime * speed));
+        }
+    }
+}
