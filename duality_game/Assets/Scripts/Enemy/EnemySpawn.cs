2f3ef329cb44fd18bba1cb2fb8d329dc30a3ef90
CormacDC <60583567+CormacDC@users.noreply.github.com>
2022-01-29 21:56:48 -0500

    enemies now spawn in correct positions

--- duality_game/Assets/Scripts/Enemy/EnemySpawn.cs
+++ duality_game/Assets/Scripts/Enemy/EnemySpawn.cs
@@ -5,41 +5,42 @@ public class EnemySpawn : MonoBehaviour
     public GameObject enemy;
     private GameObject newEnemy;
     private SpriteRenderer rend;
-    private int randomSpawnZone;
-    private float randomX, randomY;
-    private Vector3 spawnPos;
+    private Vector3 spawnPos, potentialSpawn1, potentialSpawn2, potentialSpawn3, potentialSpawn0;
+
+    public Camera MainCamera;
+    private Vector2 screenBounds;
 
     // Start is called before the first frame update
     void Start()
     {
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+
+        potentialSpawn0 = new Vector3(screenBounds.x + 1, screenBounds.y - (screenBounds.y * (1f/5f) * 2f), 0f);
+        potentialSpawn1 = new Vector3(screenBounds.x + 1, screenBounds.y - (screenBounds.y * (2f/5f) * 2f), 0f);
+        potentialSpawn2 = new Vector3(screenBounds.x + 1, screenBounds.y - (screenBounds.y * (3f/5f) * 2f), 0f);
+        potentialSpawn3 = new Vector3(screenBounds.x + 1, screenBounds.y - (screenBounds.y * (4f/5f) * 2f), 0f);
+
         InvokeRepeating("SpawnNewEnemy", 0f, 2f);
     }
 
     private void SpawnNewEnemy()
     {
-        randomSpawnZone = Random.Range(0,4);
-
-        switch (randomSpawnZone)
+        switch (Random.Range(0,4))
         {
             case 0:
-                    randomX = Random.Range(-11f, -10f);
-                    randomY = Random.Range(-10f, 10f);
+                    spawnPos = potentialSpawn0;
                     break;
             case 1:
-                    randomX = Random.Range(-10f, 10f);
-                    randomY = Random.Range(-7f, -8f);
+                    spawnPos = potentialSpawn1;
                     break;
             case 2:
-                    randomX = Random.Range(10f, 11f);
-                    randomY = Random.Range(-8f, 8f);
+                    spawnPos = potentialSpawn2;
                     break;
             case 3:
-                    randomX = Random.Range(-10f, 10f);
-                    randomY = Random.Range(7f, 8f);
+                    spawnPos = potentialSpawn3;
                     break;
         }
 
-        spawnPos = new Vector3(randomX, randomY, 0f);
         newEnemy = Instantiate(enemy, spawnPos, Quaternion.identity);
         rend = newEnemy.GetComponent<SpriteRenderer>();
         switch (Random.Range(0,2))
