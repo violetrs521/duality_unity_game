ee718d89c9d48d76bb3dfe2bdb28984c1829508d
CormacDC <60583567+CormacDC@users.noreply.github.com>
2022-01-29 23:29:52 -0500

    enemies now move across the screen

--- duality_game/Assets/Scripts/Enemy/EnemyMovement.cs
+++ duality_game/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class EnemyMovement : MonoBehaviour
 {
+    public float enemySpeed;
+    private Rigidbody2D enemyRigidBody;
+    private float newEnemyXPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemySpeed = 10f;
+
+        enemyRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        newEnemyXPosition = transform.position.x - enemySpeed * Time.deltaTime;
+        transform.position = new Vector3(newEnemyXPosition, transform.position.y, transform.position.z);
     }
 }
