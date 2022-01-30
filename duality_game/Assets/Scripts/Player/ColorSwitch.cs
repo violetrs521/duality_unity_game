1d149f3804ee38beb04502b053cd0660099a1505
chris-m-taylor <chrismtaylor1998@gmail.com>
2022-01-29 20:43:34 -0500

    Refactor ColorSwitch.cs

--- duality_game/Assets/Scripts/Player/ColorSwitch.cs
+++ duality_game/Assets/Scripts/Player/ColorSwitch.cs
@@ -7,26 +7,22 @@ public class ColorSwitch : MonoBehaviour
     // Start is called before the first frame update
     SpriteRenderer sprite;
     
-    
     void Start()
     {
-       
-       
-        
         sprite = GetComponent<SpriteRenderer>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        if((Input.GetKeyDown(KeyCode.Space))& sprite.color == Color.black){
-            
-            sprite.color = new Color(1, 1, 1, 1);
+        if((Input.GetKeyDown(KeyCode.Space))& sprite.color == Color.black)
+        {
+            sprite.color = Color.white;
         }
 
-        else if((Input.GetKeyDown(KeyCode.Space))& sprite.color == Color.white){
-            
-            sprite.color = new Color(0, 0, 0, 1);
+        else if((Input.GetKeyDown(KeyCode.Space))& sprite.color == Color.white)
+        {
+            sprite.color = Color.black;
         }
     }
 }
